3d741863bcf2189d31ca797487ed903d19bb2a94
Aidar <c>
2020-04-26 22:16:30 +0300

    docker for service app

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,28 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	confluentkafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/im-so-sorry/streaming-vk/pkg/brokers/kafka"
 	"github.com/im-so-sorry/streaming-vk/pkg/vk"
-	confluentkafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 type CommandContent struct {
 	CommandType string `json:"command_type"`
-	Rule vk.Rule
+	Rule        vk.Rule
 }
 
 type MessageMeta struct {
-
 }
 
 type CommandMessage struct {
 	Content CommandContent `json:"content"`
-	Meta MessageMeta `json:"meta"`
+	Meta    MessageMeta    `json:"meta"`
 }
 
 type CommandResponse struct {
-	
 }
 
 type ResponseMessage struct {
@@ -31,25 +31,32 @@ type ResponseMessage struct {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	vkToken := os.Getenv("VK_TOKEN")
+	kafkaHost := os.Getenv("KAFKA_HOST")
+	kafkaProduceTopic := os.Getenv("KAFKA_COMMAND_RESULT_TOPIC")
+	kafkaCommandTopic := os.Getenv("KAFKA_COMMAND_TOPIC")
+
 	consumer := kafka.Consumer{}
 	producer := kafka.Producer{}
 
-	resultTopic := "vk.command.result"
-
-	producer.Initialize("localhost:9093", 0)
-	consumer.Initialize("localhost:9093", "vk.command", []string{"vk.command"}, 0)
+	producer.Initialize(kafkaHost, 0)
+	consumer.Initialize(kafkaHost, "vk_command", []string{kafkaCommandTopic}, 0)
 
 	client := vk.Client{}
 
-	client.Initialize("")
+	client.Initialize(vkToken)
 
 	auth, _ := client.GetServerUrl()
 
 	messageChan := make(chan *confluentkafka.Message)
 
-
 	go func() {
-		var cmd  CommandMessage
+		var cmd CommandMessage
 		for message := range messageChan {
 
 			if err := json.Unmarshal(message.Value, &cmd); err != nil {
@@ -58,14 +65,31 @@ func main() {
 
 			switch cmd.Content.CommandType {
 			case "list":
-				if rules, err := client.GetRules(auth); err != nil {
-					json.Marshal(rules)
-					producer.Produce())
+				if rules, err := client.GetRules(auth); err == nil {
+					msg, _ := json.Marshal(rules)
+					producer.Produce(msg, kafkaProduceTopic)
+				} else {
+					log.Fatal(err)
 				}
 			case "add":
-				client.AddRule(auth, cmd.Content.Rule)
+				response, err := client.AddRule(auth, cmd.Content.Rule)
+
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				msg, _ := json.Marshal(response)
+				producer.Produce(msg, kafkaProduceTopic)
+
 			case "remove":
-				client.RemoveRule(auth, cmd.Content.Rule.Tag)
+				response, err := client.RemoveRule(auth, cmd.Content.Rule.Tag)
+
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				msg, _ := json.Marshal(response)
+				producer.Produce(msg, kafkaProduceTopic)
 			default:
 				log.Println("Unknown command type:", cmd.Content.CommandType)
 			}
